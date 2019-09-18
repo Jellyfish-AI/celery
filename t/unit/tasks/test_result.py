751966f24d80dddc4c78c6a3289d82fab8f9e25e
gsfish <caoyu97@hotmail.com>
2019-09-18 18:52:19 +0800

    celery#5736, Fix disable_sync_subtasks not respected in ResultSet (#5737)
    * celery#5736, Fix disable_sync_subtasks not respected in ResultSet
    
    * Add test_get_sync_subtask_option for ResultSet

--- t/unit/tasks/test_result.py
+++ t/unit/tasks/test_result.py
@@ -453,6 +453,17 @@ class test_ResultSet:
         b.supports_native_join = True
         x.get()
         x.join_native.assert_called()
+    
+    @patch('celery.result.task_join_will_block')
+    def test_get_sync_subtask_option(self, task_join_will_block):
+        task_join_will_block.return_value = True
+        x = self.app.ResultSet([self.app.AsyncResult(str(t)) for t in [1, 2, 3]])
+        b = x.results[0].backend = Mock()
+        b.supports_native_join = False
+        with pytest.raises(RuntimeError):
+            x.get()
+        with pytest.raises(TimeoutError):
+            x.get(disable_sync_subtasks=False, timeout=0.1)
 
     def test_eq_ne(self):
         g1 = self.app.ResultSet([
